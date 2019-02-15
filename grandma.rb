6511b586b364630b006acbc83d06a18d3bcc9cbd
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-15 19:53:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if "#{phrase}"
-    return "#{phrase}"
-  end
-end
+  if phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  else
+    return 
