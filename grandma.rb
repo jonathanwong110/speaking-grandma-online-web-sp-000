c689ca4d9500d4d88ed3a49aacb8b3b71ac85c3a
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-15 20:00:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,9 +19,7 @@ def speak_to_grandma(phrase)
       return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
-    else
+    else phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
     end
 end
