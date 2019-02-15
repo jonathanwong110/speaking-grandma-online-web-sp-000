818aee0c0057a15134b2831c42331b09ef813d97
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-15 19:43:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,15 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma
-  if "I LOVE YOU GRANDMA"
+  if puts "I LOVE YOU GRANDMA"
     return "I LOVE YOU TOO PUMPKIN"
-  elsif "Hi, Nana, how are you?" || "Hi!"
+  elsif puts "Hi, Nana, how are you?" || "Hi!"
     return "HUH?! SPEAK UP, SONNY!"
-  elsif "Hi!"
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif "WHAT DID YOU EAT TODAY?"
-    return "NO, NOT SINCE 1938!"
-  else "WHAT?"
+  else puts "WHAT DID YOU EAT TODAY?" || "WHAT?"
     return "NO, NOT SINCE 1938!"
   end
 end
