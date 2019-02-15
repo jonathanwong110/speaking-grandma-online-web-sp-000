44bba4fc63f632a281ed95efaa163d1bb1e47089
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-15 19:58:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,15 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
-    return "HUH?! SPEAK UP SONNY!"
-  else phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  end
+    if phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase == phrase.downcase
+      return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == phrase.downcase
+      return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == phrase.upcase
+      return "NO, NOT SINCE 1938!"
+    else
+      return "NO, NOT SINCE 1938!"
+    end
 end
